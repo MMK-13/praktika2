dac711eda073da9099165e28d3add097300e886c
MMK-13 <kantemamadou13@gmail.com>
2022-09-24 13:23:56 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<TopHeader />
+	<GameList />
+</template>
+
+<script>
+	import TopHeader from './components/TopHeader.vue'
+	import GameList from './components/GameList.vue'
+	
+	export default {
+		name: 'App',
+		components: {
+			TopHeader,
+			GameList
+		}
+	}
+</script>
+
+<style>
+	#app {
+		
+	}
+</style>
